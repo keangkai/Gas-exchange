04ade0de9ccb6e260cd9eedf4d5188e3b721e47c
Keangkai Poungmanee <m.keangkai@gmail.com>
2019-08-02 12:06:16 +0700

    Add frontend files

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,18 @@ module.exports = {
                host: "localhost",
                port: 7545,
                network_id: "*" // Match any network id
+          },
+          chainskills: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4224",
+            gas: 4700000
+          },
+          rinkeby: {
+               host: "localhost",
+               port: 8545,
+               network_id: "4",
+               gas: 4700000
           }
      }
 };
--- truffle.js
+++ truffle.js
@@ -12,6 +12,20 @@ module.exports = {
             port: 8545,
             network_id: "4224",
             gas: 4700000
+          },
+          rinkeby: {
+               host: "localhost",
+               port: 7545,
+               network_id: "4",
+               gas: 4700000
+          },
+          live: {
+               host: "localhost",
+               port: 8545,
+               network_id: "1",
+               gas: 4700000,
+               gasPrice: 57000000000,
+               from : "0x076dfc548f64b00f23c330d2a2b1ea471e704b64"
           }
      }
 };
